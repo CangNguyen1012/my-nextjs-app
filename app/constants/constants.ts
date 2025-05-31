f6f16796e45d7d413c003c251a9dde71eecf3638
CangNguyen <brunomessi933@gmail.com>
2025-05-31 09:53:14 +0700

    setup

---
+++ app/constants/constants.ts
@@ -0,0 +1,57 @@
+// import vcbLOGO from "@/assets/vcb-logo.png";
+// import vnpayLogo from "@/assets/vnpay.svg";
+
+// export const ENV = {
+//   NODE_ENV: import.meta.env.MODE,
+//   apiHost: import.meta.env.VITE_API_HOST,
+//   socketHost: import.meta.env.VITE_SOCKET_HOST,
+//   frontEndHost: `${window.location.protocol}//${window.location.hostname}${
+//     window.location.port ? `:${window.location.port}` : ""
+//   }`,
+//   SHOW_SKIP_PAYMENT: import.meta.env.VITE_SHOW_SKIP_PAYMENT === "true",
+// };
+// export const FORMAT_DATE_TIME = "YYYY-MM-DD HH:mm:ss";
+
+// export const FORMAT_DATE = "YYYY-MM-DD";
+
+// export const FORMAT_DATE_2 = "DD/MM/YYYY";
+
+// export const statusPayment = {
+//   0: { text: "Gián đoạn", color: "orange" },
+//   1: { text: "Hoàn thành", color: "green" },
+//   "-1": { text: "Thất bại", color: "red" },
+// };
+
+// export const JOBMODE_OPTION = [
+//   { value: "*", label: "Tất cả" },
+//   { value: "LAYN", label: "Lấy nguyên" },
+//   { value: "HBAI", label: "Hạ bãi" },
+//   { value: "TRAR", label: "Trả rỗng" },
+//   { value: "CAPR", label: "Cấp rỗng" },
+// ];
+// export const JOBMODE_VALUE = { LAYN: "Lấy nguyên", HBAI: "Hạ bãi", TRAR: "Trả rỗng", CAPR: "Cấp rỗng" };
+
+// export const STATUS_ORDER_OPTION = [
+//   { value: 0, label: "Chờ nhận", color: "lime" },
+//   { value: 1, label: "Sẵn sàng", color: "lime" },
+//   { value: 2, label: "Vào cổng", color: "cyan" },
+//   { value: 3, label: "Ra cổng", color: "cyan" },
+//   { value: 4, label: "Trả hàng", color: "cyan" },
+//   { value: 5, label: "Hủy", color: "red" },
+//   { value: 6, label: "Hoàn tất", color: "green" },
+// ];
+
+// export const regExpCheckSpecialChar = new RegExp("[~|`|!|@|#|$|%|^|&|*|(|)|/]", "g");
+
+// export const TIMEZONE_DEFAULT = "Asia/Ho_Chi_Minh";
+
+// export const moneyCreditOption = [
+//   { value: "M", label: "Thu ngay" },
+//   { value: "C", label: "Thu sau" },
+// ];
+
+// export const bankTestList = ["VCB"];
+// export const bankPaymentOptions = [
+//   { bankCode: "VNPAY", text: "VNPAY", image: vnpayLogo, qrCode: "" },
+//   { bankCode: "VCB", text: "Thanh toán bằng VCB", image: vcbLOGO, qrCode: "" },
+// ];
